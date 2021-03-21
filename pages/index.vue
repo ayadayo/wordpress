07c4615816eeb01df761ee5806b3ebd6eb8b4aaa
塚原彩 <ayatsukahara0112@gmail.com>
2021-03-21 16:31:37 +0900

    .ファイル移動

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">app</h1>
+      <h1 class="title">app1</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
